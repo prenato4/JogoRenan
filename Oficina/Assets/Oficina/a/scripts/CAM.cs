2a9dbd46e17626122f5ab0bbc40e1cfbdc887bff
Pedro Renato Dos Anjos Silva <20211170150017@ifrn.local>
2023-08-02 14:44:41 -0300

    collisors

--- Oficina/Assets/Oficina/a/scripts/CAM.cs
+++ Oficina/Assets/Oficina/a/scripts/CAM.cs
@@ -17,22 +17,20 @@ public class CAM : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if (player.position.x >= -5.0 && player.position.x < 659.0)
-        {
-            Vector3 following = new Vector3(player.position.x,player.position.y,transform.position.z);
 
-            transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
-        
-        }
-        
-        /*if (player.position.x < 285)
-        {
-            Vector3 following = new Vector3(player.position.x, player.position.y, transform.position.z);
+        Vector3 following = new Vector3(player.position.x, player.position.y, transform.position.z);
 
-            transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
-        
-        }*/
+        transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
+    }
+
+    /*if (player.position.x < 285)
+    {
+        Vector3 following = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
+    
+    }*/
       
         
     }
-}
+
